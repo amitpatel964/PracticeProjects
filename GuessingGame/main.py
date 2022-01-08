47c6df4b3685d4159e96584c11c69d3a0ff12f68
amitpatel964 <70494546+amitpatel964@users.noreply.github.com>
2022-01-08 11:48:28 -0500

    Add files via upload

--- GuessingGame/main.py
+++ GuessingGame/main.py
@@ -14,6 +14,31 @@ def guess():
 
     print("That's the right answer!")
 
+def guessComputer():
+    numberOfGuesses = 0
+    lowerBound = int(input("What do you want the lower bound to be? "))
+    upperBound = int(input("What do you want the upper bound to be? "))
+
+    numberToGuess = int(input("What number do you want the computer to guess? "))
+
+    while numberToGuess < lowerBound or numberToGuess > upperBound:
+        numberToGuess = int(input("What number do you want the computer to guess? "))
+
+    guessedNumber = random.randint(lowerBound, upperBound)
+    numberOfGuesses += 1
+    numbersGuessed = [guessedNumber]
+
+    # We want to keep track of what numbers the computer is guessing
+    # This is so the computer does not guess a number it has already guessed
+    while guessedNumber != numberToGuess:
+        print("The computer guessed %d, but that was not the answer" %(guessedNumber))
+        guessedNumber = random.randint(lowerBound, upperBound)
+        while guessedNumber in numbersGuessed:
+            guessedNumber = random.randint(lowerBound, upperBound)
+        numberOfGuesses += 1
+        numbersGuessed.append(guessedNumber)
+
+    print("The computer guessed %d and that was the answer! It took %d guesses" % (guessedNumber, numberOfGuesses))
 
 # Get the type of guessing game the user wants to play
 command = ""
@@ -26,3 +51,4 @@ if command == "guess":
 
 if command == "computer guess":
     print("You want the computer to guess the number")
+    guessComputer()
